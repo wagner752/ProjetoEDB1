89ea489619fac4e421e39a8a5cb815f12802a437
Washington Wagner <31409676+wagner752@users.noreply.github.com>
2019-09-18 16:58:44 -0300

    Add files via upload

--- index.cpp
+++ index.cpp
@@ -11,35 +11,13 @@
     6. arranjos com 50% de seus elementos em sua posic~ao denitiva.
 */
 
-int main(){
+//Implementacao do Insertion Sort
+void insertionSort(int *Array){
+    int k, auxiliar, n;
     
-    srand (time(NULL));
-    
-    //Variavel para armazenar o valor máximo de valores a ser armazenado
-    //de forma aleatória.
-    //Variaveis aleatórias até limiteAleatorio
-    int limiteAleatorio;
-    
-    //Auxiliares na ordenacao do insertion sort
-    //i =  indice
-    int i, k, n, auxiliar;
-    
-    //Alocacao dinamica de um ponteiro que aponta para um array de 16 posicoes
-    //Esse valor devera ser modificado para o valor máximo que a memória consegue
-    //alocar
-    int *Array = new int [100000];
-    
-    //Definindo valores aleatórios em um vetor de 100000 posições, os valores
-    //vao de 1 até 666.
-    for(i = 0 ; i<= 100000; i++){
-        limiteAleatorio = rand() % 666 + 1;
-        Array[i] = limiteAleatorio;
-    }
-    
-    //Implementacao do Insertion Sort
         //Percorre todo o vetor criado (Array)
     std::cout << "Ordenando com Insertion sort" << std::endl;
-    for(k = 1; k<= 100000 ; k++){
+    for(k = 1 ; k<= 100 ; k++){
         auxiliar = Array[k];
         n = k - 1;
 
@@ -52,20 +30,24 @@ int main(){
         //Próximo elemento do velor;
         Array[n+1] =  auxiliar;
     }
-    //Fim Insertion Sort
-    /*
-    //Imprime os valores na tela ordenados através do Insertion Sort
-    std::cout << "Os valores foram ordenados atraves do Insertion Sort: " << std::endl;
-    for (i = 0 ; i <= 100000 ; i++){
-        std::cout << Array[i] << " ";
-    }
-    */
-    //Implementacao do Bubble Sort
-        //Percorre todo o vetor criado (Array)
+}
+//Fim Insertion Sort
+
+//Implementacao do Selection Sort
+void selectionSort(){
+
+}
+//Fim Selection Sort
+
+//Implementacao do Bubble Sort
+void bubbleSort(int *Array){
+    int k, n, auxiliar;
+    
+    //Percorre todo o vetor criado (Array)
     std::cout << "Ordenando com Bubble sort" << std::endl;
-    for(k = 1; k < 100000 ; k++){
+    for(k = 1 ; k < 100 ; k++){
         //Percorrer o vetor procurando os pares para fazermos a troca
-        for(n = 0; n < 100000 - k ; n++){
+        for(n = 0; n < 100 - k ; n++){
             //Se o valor comparado for menor que o próximo valor
             //será realizada a troca 
             if(Array[n] > Array[n+1]){
@@ -75,17 +57,144 @@ int main(){
             }
         }
     }
-    //Fim Bubble Sort
+}
+//Fim Bubble Sort
 
-    //Implementacao do Quick sort
+//Implementacao do Quick sort
+void quickSort(int *Array){
+    int k, pivot, auxiliar, i, n;
+    //Percorre todo o vetor criado (Array)
+    for(k = 1 ; k < 100000 ; k++){
+        //Enquanto o K não chega ao final do vetor
+        if(k < 100000){
+            pivot = k;
+            i = k;
+            n = 100000;
+            //Enquanto i não chega ao final do vetor
+            while(i < n){
+                while(Array[i] <= Array[pivot] && i < 100000){
+                    i++;
+                }
+                while(Array[n] > Array[pivot]){
+                    n--;
+                }
+                //Swap para realizar a troca das posições
+                if(i < n){
+                    auxiliar = Array[i];
+                    Array[i] = Array[n];
+                    Array[n] = auxiliar;
+                }
+            }
+            //Swap entre o pivot e o array
+            auxiliar = Array[pivot];
+            Array[pivot] = Array[n];
+            Array[n] = auxiliar;
+        }
+    }
+}
+//Fim Quick Sort
 
+//Implementacao do Merge Sort
+void mergeSort(int *Array){
+    void sort(int L, int R){
+        //L = Left
+        //R = RIGHT
+        //M = MIDDLE
+        int M;
+
+        //Divisao do Merge Sort
+        if(L < R){
+            M = (L + R) / 2;
+
+        }
+        //Ocorre a recursividade até a separação total dos elementos
+        sort(L,R);
+    }
+}
+
+int main(){
     
-    //Imprime os valores na tela ordenados através do Bubble Sort
-    std::cout << "\nOs valores foram ordenados atraves do Bubble Sort: " << std::endl;
+    srand (time(NULL));
     
+    //Variavel para armazenar o valor máximo de valores a ser armazenado
+    //de forma aleatória.
+    //Variaveis aleatórias até limiteAleatorio
+    int limiteAleatorio;
+    
+    //Auxiliares na ordenacao do insertion sort
+    //i =  indice
+    int unsigned i;
+    int k, n, auxiliar, pivot, selecao;
+    
+    //Alocacao dinamica de um ponteiro que aponta para um array de 16 posicoes
+    //Esse valor devera ser modificado para o valor máximo que a memória consegue
+    //alocar
+    int *Array = new int [100];
+    
+    //Definindo valores aleatórios em um vetor de >>>100<<< posições, os valores
+    //vao de 1 até 666.
+    for(i = 0 ; i<= 100; i++){
+        limiteAleatorio = rand() % 100 + 1;
+        Array[i] = limiteAleatorio;
+    }
+
+    //Menu de selecao do algoritmo de ordenacao
+    std::cout << "Selecione o algoritmo de ordenacao\n" 
+                 "1) Insertion Sort\n" 
+                 "2) Selection Sort\n"
+                 "3) Bubble Sort\n"
+                 "4) Shell Sort\n"
+                 "5) Quick Sort\n"
+                 "6) Merge Sort\n"
+                 "7) Radix Sort" << std::endl;
+    std::cin >> selecao;
+
+    //Selecao do Insertion Sort
+    if (selecao == 1){
+        //Ordena os valores através do Insertion Sort    
+        insertionSort(Array);
+        std::cout << "O arranjo foi ordenado com sucesso atraves do Insertion Sort\n" << std::endl;
+    }
+
+    //Selecao do Selection Sort
+    if (selecao == 2){
+        // ======================SELECTION SORT=================
+    }
+
+    //Selecao do Bubble Sort
+    if (selecao == 3){
+        //Ordena os valores através do Insertion Sort    
+        bubbleSort(Array);
+        std::cout << "O arranjo foi ordenado com sucesso atraves do Bubble Sort\n" << std::endl;
+    }
+
+    //Selecao do Shell Sort
+    if(selecao == 4){
+        // ======================Shell SORT=================
+    }
+
+    //Selecao do Quick Sort
+    if(selecao == 5){
+        quickSort(Array);
+        std::cout << "O arranjo foi ordenado com sucesso atraves do Quick Sort\n" << std::endl;
+    }
+    
+    //Selecao do Merge Sort
+    if(selecao == 6){
+        // ======================MERGE SORT=================  
+    }
+
+    //Selecao do Radix Sort
+    if(selecao == 7){
+        // ======================RADIX SORT=================
+    }
+
+    /*
+    //IMPRIMIR RESULTADO
     for (i = 0 ; i <= 100000 ; i++){
         std::cout << Array[i] << " ";
     }
-    
+    */
+
     return 0;
 }
